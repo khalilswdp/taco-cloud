9dc5ba30fa7e53d7bb7f858e3a809161c3ec2bd8
Khalil SLEIMI <khalil.swdp+oss.github@gmail.com>
2022-06-02 14:25:54 +0100

    Use lambda expression as mapper

--- src/main/java/sia/tacocloud/data/JdbcIngredientRepository.java
+++ src/main/java/sia/tacocloud/data/JdbcIngredientRepository.java
@@ -29,15 +29,10 @@ public class JdbcIngredientRepository implements IngredientRepository {
     @Override
     public Ingredient findOne(String id) {
         return jdbc.queryForObject("select id, name, type from Ingredient where id=?",
-                new RowMapper<Ingredient>() {
-                    @Override
-                    public Ingredient mapRow(ResultSet rs, int rowNum) throws SQLException {
-                        return new Ingredient(
-                                rs.getString("id"),
-                                rs.getString("name"),
-                                Ingredient.Type.valueOf(rs.getString("type")));
-                    }
-                }
+                (rs, rowNum) -> new Ingredient(
+                        rs.getString("id"),
+                        rs.getString("name"),
+                        Ingredient.Type.valueOf(rs.getString("type")))
                 , id);
     }
 
