4877ff8e2a2e5fa2ee7fbfa21a8acd819bfc3a1a
Khalil SLEIMI <khalil.swdp+oss.github@gmail.com>
2022-06-03 10:16:15 +0100

    Add another method to OrderRepository

--- src/main/java/sia/tacocloud/data/OrderRepository.java
+++ src/main/java/sia/tacocloud/data/OrderRepository.java
@@ -3,9 +3,12 @@ package sia.tacocloud.data;
 import org.springframework.data.repository.CrudRepository;
 import sia.tacocloud.Order;
 
+import java.util.Date;
 import java.util.List;
 
 public interface OrderRepository extends CrudRepository<Order, Long> {
 
     List<Order> findByDeliveryZip(String deliveryZip);
+
+    List<Order> readOrdersByDeliveryZipAndPlacedAtBetween(String deliveryZip, Date startDate, Date endDate);
 }
