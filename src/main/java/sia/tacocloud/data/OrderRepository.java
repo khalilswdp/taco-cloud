c6dcbcda58a59bc302a8dfc8292500bfdab096cc
Khalil SLEIMI <khalil.swdp+oss.github@gmail.com>
2022-06-03 10:42:54 +0100

    Add methods to repository to showcase possibilities in naming conventions

--- src/main/java/sia/tacocloud/data/OrderRepository.java
+++ src/main/java/sia/tacocloud/data/OrderRepository.java
@@ -1,5 +1,6 @@
 package sia.tacocloud.data;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import sia.tacocloud.Order;
 
@@ -11,4 +12,9 @@ public interface OrderRepository extends CrudRepository<Order, Long> {
     List<Order> findByDeliveryZip(String deliveryZip);
 
     List<Order> readOrdersByDeliveryZipAndPlacedAtBetween(String deliveryZip, Date startDate, Date endDate);
+
+    List<Order> findByDeliveryCityOrderByNameAllIgnoreCase(String deliveryCity);
+
+    @Query("SELECT o FROM Order o where o.deliveryCity='Seattle'")
+    List<Order> readOrdersDeliveredInSeattle();
 }
